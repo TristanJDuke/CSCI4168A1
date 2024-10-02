dbb9d9d5cf1ac48b1ec3cfb23f168a4c5e943674
Tristan <43221884+TristanJDuke@users.noreply.github.com>
2024-10-02 15:53:48 -0300

    Added beginnings of health and UI elements

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -1,18 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerCharacter : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float maxHealth = 100;
+    private HealthBar healthBar;
+    public static PlayerCharacter player { get; private set; }
+    public float currentHealth { get; private set; }
+    public Slider healthSlider;
     void Start()
     {
-        
+        player = this;
+        healthBar = PlayerCharacter.player.healthSlider.GetComponent<HealthBar>();
+        healthBar.SetMaxHealth(maxHealth);
+        currentHealth = maxHealth;
     }
+    public void TakeDamage(float damage) {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+        if (currentHealth <= 0) {
+            Destroy(this.gameObject);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
